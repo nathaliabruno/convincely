0b7ff20800092da39341c98a10a0e622e2d14833
James Hunt <james.hunt@convincely.com>
2021-11-10 17:48:14 +1100

    Init

---
+++ src/Movies/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from "axios";
+// import _ from "lodash";
+
+const url = 'https://the-one-api.dev/v2/movie';
+const apiToken = 'jhnSIPP46DZsewnkbDgl';
+
+const getMovies = async () => axios.get(url, {
+		headers: {
+			Authorization: `Bearer ...`
+		}
+	}).then((response) => {
+		console.log(response, "response");
+	});
+
+const Movies = () => {
+	return (
+		<div className="challenge">Insert code here ... good luck &#128540;!</div>
+	);
+}
+
+export default Movies
